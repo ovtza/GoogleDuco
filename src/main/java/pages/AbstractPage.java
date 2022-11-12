017c9b40e954330f1b45c663d6559b7532ce44c8
ovtza <szymonowca@gmail.com>
2022-11-12 20:45:25 +0100

    Add AbstractPage and automated driver management

--- src/main/java/pages/AbstractPage.java
+++ src/main/java/pages/AbstractPage.java
@@ -1,17 +1,51 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public abstract class AbstractPage {
 
     WebDriver driver;
+    final By searchInput = By.name("q");
+    final By acceptTermsButton = By.cssSelector("button[id='L2AGLb'] div[role='none']");
+    final By googleKeyBoardElm = By.xpath("(//span[@class='ly0Ckb'])[1]");
 
     public AbstractPage(WebDriver driver){
-        System.setProperty("webdriver.chrome.driver", "C:/chromedrivers/chromedriver.exe");
         this.driver = driver;
+    }
+
+    public void setPolishLanguage(){
+        driver.get("https://www.google.com/preferences#languages");
+        driver.findElement(By.xpath("//div[@data-value='pl']//span[@class='jfk-radiobutton-radio']")).click();
+        driver.findElement(By.xpath("//div[@class='goog-inline-block jfk-button jfk-button-action']")).click();
+        driver.switchTo().alert().accept();
+    }
+
+    public void typeSearchTerm(String term){
+        driver.findElement(searchInput).sendKeys(term);
+    }
+
+    public void clickOnAcceptTermsButton(){
+        driver.findElement(acceptTermsButton).click();
     }
 
     public void isAt(String url){
         driver.get(url);
+    }
+
+    public String getUrl(){
+        return driver.getCurrentUrl();
+    }
+
+    public String getTitle(){
+        return driver.getTitle();
+    }
+
+    public void clickOnKeyboardElm(){
+        driver.findElement(googleKeyBoardElm).click();
+    }
+
+    public By getSearchInput(){
+        return searchInput;
     }
 }
